d35542578e93325514026029fc4c650cad142fd5
Otto Lutz <ottolutz00@gmail.com>
2021-09-02 21:44:22 -0400

    Initial Migration

--- ElevenNote/ElevenNote.Models/NoteCreate.cs
+++ ElevenNote/ElevenNote.Models/NoteCreate.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteCreate
+    //model for the user enter into when creating a note, we can't and won't need the user to provide everything, we just want the title and content, not the id, datecreated, or the owner id
+    public class NoteCreate
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+
     }
 }
---
+++ ElevenNote/ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,55 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        private NoteService CreateNoteService()
+        {
+            // GetUserId will do the work for me and get the user's access token
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            // the user will have access to the CRUD methods because the class containing them will have been passed in the userId
+            var noteService = new NoteService(userId);
+            // returns the NoteService object that let's the user access and change data
+            return noteService;
+        }
+
+        // GET
+        public IHttpActionResult Get()
+        {
+            //authorizes the user and gives 'noteService' object access to the CRUD methods
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        // POST
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            NoteService service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+
+
+
+    }
+}
